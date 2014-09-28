e22dc9e9ba5880e797e758429d8c343b76ce9aa1
Nikos Vasilakis <nikos.ailo@gmail.com>
2014-09-28 12:02:19 -0400

    [Minor] Updates to scripts

--- scripts/trymenode.sh
+++ scripts/trymenode.sh
@@ -3,34 +3,41 @@
 ##
 # Bring a checked out template repository to a working form
 #
-# Usage: ./trymenode
+# Usage: source this file and run install-app
 ##
 
 REDIS_VERSION="2.8.17"
+APP_NAME="kangaroo"
 
 function epilogue {
-  echo "Change the name of the project, edit .git/config, and commit changes"
+  echo "Run 'launch-app'"
 }
 
-function install_redis {
+function install-redis {
   wget http://download.redis.io/releases/redis-${REDIS_VERSION}.tar.gz
-  tar xzf redis-${REDIS_VERSION}.tar.gz redis/
-  cd redis/
+  tar xzf redis-${REDIS_VERSION}.tar.gz 
+  cd redis*/
   make
   echo "To run redis, go to  'redis-${REDIS_VERSION}/src' and type './redis-server &'"
   echo "For the console, type './redis-cli'; to shutdown, type './redis-cli shutdown'"
 }
 
-function execute {
+function install-app {
+  clone-app
+  cd ${APP_NAME}
+  install-redis
   npm install
-  rm $0
-  
-  echo "Should we launch app? [Y/n]"
-  read repl
   epilogue
-  if [[ $repl == "n" ]]; then
-    exit
-  fi
-  
+}
+
+function clone-app {
+  git clone git@github.com:nvasilakis/${APP_NAME}.git
+}
+
+function launch-app {
+  pwd
+  cd ${APP_NAME}
+  ./redis/src/redis-server &
+  ./scripts/populateDB.js
   node app.js
 }
